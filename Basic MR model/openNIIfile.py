013563b7e74ff28f9c857cc82349e8b417958f4d
vimuth97 <47806934+vimuth97@users.noreply.github.com>
2021-06-12 18:49:19 +0530

    removed unwanted files

--- Basic MR model/openNIIfile.py
+++
@@ -1,9 +0,0 @@
-# conda install -c conda-forge nibabel
-
-import nibabel as nib
-import os
-
-
-file = os.path.join("C:/Users/user/Documents/FYP test images", 'test.nii.gz')
-img = nib.load(file)  # LOAD .nii IMAGE
-print(img.shape)
--- Basic MR model/openTifPIL.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import numpy
-
-# im = Image.open('C:/Users/user/Documents/FYP test images/file_example_TIFF_10MB.tiff')
-im = Image.open('C:/Users/user/Documents/FYP test images/test.tiff')
-imarray = numpy.array(im)
-imarray.shape
-im.show()
